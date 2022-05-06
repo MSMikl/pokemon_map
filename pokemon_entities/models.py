4d9bc2aa6b3b23478aa6c9efd49132bfb2489840
MSMikl <77015283+MSMikl@users.noreply.github.com>
2022-05-06 11:24:27 +0300

    add PokemonEntity model

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -8,3 +8,12 @@ class Pokemon(models.Model):
     
     def __str__(self):
         return self.title
+
+
+class PokemonEntity(models.Model):
+    id = models.AutoField(primary_key=True, unique=True)
+    lattitude = models.FloatField('Lat.')
+    longitude = models.FloatField('Lon.')
+    
+    def __str__(self):
+        return '{}, {}'.format(self.lattitude, self.longitude)
